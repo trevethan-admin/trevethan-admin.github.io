47d504979e7536766906979fe16f3ea64de23b76
Tom Rogers <tomrogers2823@gmail.com>
2016-12-24 13:14:16 +0000

    Update instant.js

--- instant.js
+++ instant.js
@@ -1,30 +1,31 @@
 ---
 ---
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  //slider funtion
-
+  // Slider funtion
   $(function(){
     $('.sliding-gallery img:gt(0)').hide();
-    setInterval(function(){$('.sliding-gallery :first-child').fadeOut(1000).next('img').fadeIn(1000).end().appendTo('.sliding-gallery');}, 5000);
+    setInterval(function() {
+      $('.sliding-gallery :first-child')
+          .fadeOut(1000)
+          .next('img')
+          .fadeIn(1000)
+          .end()
+          .appendTo('.sliding-gallery');
+    }, 5000);
   });
 
-
-  //gallery funtion
-
-  $('.mini-container li img').click(function(){
-    $('.main-pic').attr('src',$(this).attr('src').replace('thumb','large'));
+  // Gallery funtion
+  $('.mini-container li img').click(function() {
+    $('.main-pic').attr('src', $(this).attr('src').replace('thumb','large'));
   });
 
-  //responsive menu function
-
-  $('.menubutton').click(function(){
-
-  $('.menu').toggle();
-  $('#menubutton').toggleClass('button');
-  $('#menubutton').toggleClass('button1');
-
+  // Responsive menu function
+  $('.menubutton').click(function() {
+    $('.menu').toggle();
+    $('#menubutton').toggleClass('button');
+    $('#menubutton').toggleClass('button1');
   });
 
 });
